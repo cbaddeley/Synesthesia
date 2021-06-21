8fac4fb920c3e8136b0e0e3c0815a768f06eb252
zjsimmons13 <43621406+zjsimmons13@users.noreply.github.com>
2021-06-21 17:33:55 -0400

    Add files via upload

---
+++ src/synesthesia/art_generator/MusicParsing.py
@@ -0,0 +1,78 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import QEventLoop, pyqtSignal, pyqtSlot
+import wsl
+from QtDrawing import Window
+from pydub import AudioSegment
+import pyaudio
+import librosa
+import numpy as np
+import wsl
+import wave
+import time
+
+
+def done():
+    return
+
+
+def main():
+    sound = AudioSegment.from_mp3("mp3_around_the_sun.mp3")
+    sound.export("around_the_sun.wav", format="wav")
+    wf = wave.open("around_the_sun.wav")
+
+    # Start Cavnas
+    wsl.set_display_to_host()
+    app = QApplication([])
+    window = Window()
+    window.show()
+
+    # instantiate PyAudio (1)
+    p = pyaudio.PyAudio()
+    loop = QEventLoop
+    loop.connect(p, pyqtSignal(done()), loop, pyqtSlot(quit()))
+
+    def lib_analysis(data):
+        s = np.abs(librosa.stft(data))
+        notes = librosa.hz_to_note(s)
+        return notes
+
+    # define callback (2)
+    def callback(in_data, frame_count, time_info, status):
+        # convert data to array
+        data = np.fromstring(in_data, dtype=np.float32)
+        bars = lib_analysis(data)
+        for bar in bars:
+            for note in bar:
+                if '-' in note:
+                    window.draw_something(note.split('-'))
+        return in_data, pyaudio.paContinue
+
+    # open stream using callback (3)
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=44100,
+                    input=True,
+                    output=True,
+                    frames_per_buffer=int(1024),
+                    stream_callback=callback)
+
+    # start the stream (4)
+    stream.start_stream()
+    loop.exec()
+    # wait for stream to finish (5)
+    while stream.is_active():
+        time.sleep(1)
+
+    # stop stream (6)
+    stream.stop_stream()
+    stream.close()
+    wf.close()
+    done()
+
+    # close PyAudio (7)
+    p.terminate()
+
+
+if __name__ == "__main__":
+    main()
--- src/synesthesia/art_generator/QtDrawing.py
+++ src/synesthesia/art_generator/QtDrawing.py
@@ -1,10 +1,15 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import Qt
+import ffmpeg
+import pydub
 import wsl
 
 
 class Window(QMainWindow):
+    x = 0
+    y = 0
+
     def __init__(self):
         super().__init__()
 
@@ -12,8 +17,8 @@ class Window(QMainWindow):
 
         top = 400
         left = 400
-        width = 800
-        height = 600
+        width = 1200
+        height = 900
 
         self.setWindowTitle(title)
         self.setGeometry(top, left, width, height)
@@ -25,18 +30,49 @@ class Window(QMainWindow):
         save_action.setShortcut("Ctrl + S")
         file_button.addAction(save_action)
         save_action.triggered.connect(self.save)
-        self.draw_something()
-
-    # makes the blank canvas
-    def paintEvent(self, event):
         canvas_creation = QPainter(self)
         canvas_creation.drawImage(self.rect(), self.image, self.image.rect())
 
+    # makes the blank canvas
+
     # does the drawing
-    def draw_something(self):
+    def draw_something(self, note=None):
         painter = QPainter(self.image)
         painter.setPen(QPen(Qt.black, 1, Qt.SolidLine, Qt.RoundCap, Qt.RoundJoin))
-        painter.drawLine(100, 100, 300, 300)
+        octave = int(note[1])
+        note = note[0]
+
+        if note == 'A':  # draw a point
+            painter.drawPoint(self.x % 1200, self.y + 750)
+            self.x += 1
+        elif note == 'B':  # draw a rectangle
+            painter.drawRect(self.x % 1200, self.y + 600, 20, 15)
+            self.x += 1
+        elif note == 'C':  # draw a circle
+            painter.drawEllipse(self.x % 1200, self.y + 450, 20, 20)
+            self.x += 1
+        elif note == 'D':  # draw a pie
+            painter.drawPie(self.x % 1200, self.y + 300, 20, 15, 30 * 16, 20 * 16)
+            self.x += 1
+        elif note == 'E':  # draw a hexagon
+            path = QPainterPath(self.x % 1200, self.y + 150)
+            path.lineTo(self.x % 1200 + 5, self.y + 150 + 8)
+            path.cubicTo(self.x % 1200 + 20, 0, self.x % 1200, self.y + 150 + 5, self.x % 1200 + 20, self.y + 20)
+            self.x += 1
+        elif note == 'F':  # draw a star
+            path = QPainterPath(self.x % 1200, self.y + 150)
+            path.lineTo(self.x % 1200 + 3, self.y + 150 + 6)
+            path.lineTo(self.x % 1200 + 6, self.y + 150)
+            path.lineTo(self.x % 1200, self.y + 150)
+        # sharp notes, don't want the sharp char to cause issues
+        elif note[0] == 'F':  # change the pen color to random color
+            colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+            i = 0
+            pen_color = QColor
+            pen_color.setNamedColor(colors[i])
+            i += 1
+            painter.setpen(pen_color)
+
         self.update()
 
     # save method
@@ -51,10 +87,3 @@ class Window(QMainWindow):
         self.image.save(file_path + ".png")
 
 
-if __name__ == "__main__":
-    wsl.set_display_to_host()
-    app = QApplication([])
-    window = Window()
-    window.show()
-
-    app.exec()
