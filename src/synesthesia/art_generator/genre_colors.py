6d5d3c106da3eb5cd61b65423715a2e7c2d4fe12
Andrew Garmon <andrew.garmon@gmail.com>
2021-06-25 17:21:13 -0400

    added functionality for passing hex values in for each genre

--- src/synesthesia/art_generator/genre_colors.py
+++ src/synesthesia/art_generator/genre_colors.py
@@ -1,6 +1,13 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
 
+def hex_to_colors(hex_vals): # takes a list of hex vals and returns a list of QColor objects
+    QColors = []
+    for hex_val in hex_vals:
+        rgb = tuple(int(hex_val[i:i+2], 16) for i in (0, 2, 4))
+        QColors.append(QColor(*rgb))
+    return QColors
+
 def getColors(genre):
 
     all_colors = [ 
@@ -26,7 +33,7 @@ def getColors(genre):
         'rock'              : [], 
         'pop'               : [], 
         'alternative'       : [], 
-        'indie'             : [QColor(244, 125, 74), QColor(225,45,90), QColor(255,236,92), QColor(0,141,203)],
+        'indie'             : ['F47D4A', 'E1315B', 'FFEC5C', '008DCB'],
         'electronic'        : [], 
         'female vocalists'  : [],
         'dance'             : [], 
@@ -78,4 +85,6 @@ def getColors(genre):
     colors = genre_colors.get(genre)
     if colors is None or colors == []:
         colors = all_colors
+    else:
+        colors = hex_to_colors(colors)
     return colors
