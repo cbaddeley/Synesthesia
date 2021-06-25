b97d225eda6420b76a63609e07d04055062348ed
Andrew Garmon <andrew.garmon@gmail.com>
2021-06-25 16:22:19 -0400

    Added shell for color schemes, currently random colors

---
+++ src/synesthesia/art_generator/genre_colors.py
@@ -0,0 +1,80 @@
+from PyQt5.QtCore import Qt
+
+def getColors(genre):
+
+    all_colors = [ 
+    Qt.white,
+    Qt.black,
+    Qt.cyan,
+    Qt.darkCyan,
+    Qt.red,
+    Qt.darkRed,
+    Qt.magenta,
+    Qt.darkMagenta,
+    Qt.green,
+    Qt.darkGreen,
+    Qt.yellow,
+    Qt.darkYellow,
+    Qt.blue,
+    Qt.darkBlue,
+    Qt.gray,
+    Qt.darkGray,
+    Qt.lightGray ]
+
+    genre_colors = {
+        'rock'              : [], 
+        'pop'               : [], 
+        'alternative'       : [], 
+        'indie'             : [],
+        'electronic'        : [], 
+        'female vocalists'  : [],
+        'dance'             : [], 
+        '00s'               : [], 
+        'alternative rock'  : [], 
+        'jazz'              : [], 
+        'beautiful'         : [],
+        'metal'             : [],
+        'chillout'          : [], 
+        'male vocalists'    : [],
+        'classic rock'      : [],
+        'soul'              : [],
+        'indie rock'        : [], 
+        'Mellow'            : [],
+        'electronica'       : [],
+        '80s'               : [], 
+        'folk'              : [], 
+        '90s'               : [],
+        'chill'             : [],
+        'instrumental'      : [], 
+        'punk'              : [], 
+        'oldies'            : [],
+        'blues'             : [], 
+        'hard rock'         : [], 
+        'ambient'           : [],
+        'acoustic'          : [], 
+        'experimental'      : [],
+        'female vocalist'   : [],
+        'guitar'            : [], 
+        'Hip-Hop'           : [],
+        '70s'               : [], 
+        'party'             : [], 
+        'country'           : [],
+        'easy listening'    : [], 
+        'sexy'              : [], 
+        'catchy'            : [],
+        'funk'              : [], 
+        'electro'           : [], 
+        'heavy metal'       : [],
+        'Progressive rock'  : [], 
+        '60s'               : [],
+        'rnb'               : [], 
+        'indie pop'         : [], 
+        'sad'               : [], 
+        'House'             : [], 
+        'happy'             : []
+        }
+
+    colors = genre_colors.get(genre)
+    if colors is None or colors == []:
+        colors = all_colors
+    return colors
